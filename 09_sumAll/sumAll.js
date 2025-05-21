de42237852afc3251f3f1631a755fd0d122ea7f3
jayrao213 <jayjohnrao@gmail.com>
2025-05-20 22:07:40 -0500

    Arrays and Loops Problems Completed

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if (typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    }
+    let sum = 0;
+    const start = Math.min(a, b);
+    const end = Math.max(a, b);
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
